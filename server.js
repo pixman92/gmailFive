39a6f52e27caaf1da77ed226b6a39d7b674915c7
pixman92 <GOtrustD@verizon.net>
2016-12-13 17:06:27 -0800

    ⛸👋 Updated with Gomix

--- server.js
+++ server.js
@@ -1,38 +1,40 @@
-// var http = require('http'),
-//     fs = require('fs');
+// // server.js
+// // where your node app starts
 
-
-// fs.readFile('./views/index.html', function (err, html) {
-//     if (err) {
-//         throw err; 
-//     }       
-//     http.createServer(function(request, response) {  
-//         response.writeHeader(200, {"Content-Type": "text/html"});  
-//         response.write(html);  
-//         response.end();  
-//     }).listen(8000);
-// });
-
-// server.js
-// where your node app starts
-
-// init project
+// // init project
 var express = require('express');
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+// // we've started you off with Express, 
+// // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
+// // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+
+// // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-// Simple in-memory store for no
 
-// listen for requests :)
-var listener = app.listen(8000, function () {
-  console.log('Your app is listening on port ' + 8000);
+// // app.get("/dreams", function (request, response) {
+// //   response.send(dreams);
+// // });
+
+// // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+// // app.post("/dreams", function (request, response) {
+// //   dreamas.push(request.query.dream);
+// //   response.sendStatus(200);
+// // });
+
+// // Simple in-memory store for now
+// // var dreams = [
+// //   "Find and count some sheep",
+// //   "Climb a really tall mountain",
+// //   "Wash the dishes"
+// // ];
+
+// // listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
